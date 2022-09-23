7a409662abb786cb66c85041c3173fc244ff42fb
jjjy879 <jjjy879@naver.com>
2022-09-23 17:26:11 +0900

    .

---
+++ src/SeptemberCTP/RemoteControlExample.java
@@ -0,0 +1,17 @@
+package SeptemberCTP;
+
+public class RemoteControlExample {
+
+	public static void main(String[] args) {
+		
+		RemoteControl remoteControl = new Television();
+		remoteControl.turnOn();
+		remoteControl.setMute(true);
+		remoteControl.setMute(false);
+		remoteControl.setVolume(-10);
+		RemoteControl.changeBattery();
+		remoteControl.turnOff();
+				
+	}
+
+}
