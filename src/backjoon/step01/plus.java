185e9cfb30128cc45ce359b85a2baef2ca01f69b
jjjy879 <jjjy879@naver.com>
2022-11-04 17:44:45 +0900

    백준 1000번 다시 해야함(scanner)

---
+++ src/backjoon/step01/plus.java
@@ -0,0 +1,21 @@
+package backjoon.step01;
+
+import java.util.Scanner;
+
+public class plus {
+
+	public static void main(String[] args) {
+
+		int A;
+		int B;
+		
+		Scanner sc1 = new Scanner(System.in);
+		Scanner sc2 = new Scanner(System.in);
+		
+		A = sc1.nextInt();
+		B = sc2.nextInt();
+		
+		System.out.println(A+B);
+	}
+
+}
