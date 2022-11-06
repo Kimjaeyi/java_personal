dd966ab50bfcf3dd5888a2b34f2859fc11e9e2e3
jjjy879 <jjjy879@naver.com>
2022-11-06 13:31:42 +0900

    숫자 입력 받아 나눗셈 연산(소수점까지 출력)

---
+++ src/backjoon/step01/division.java
@@ -0,0 +1,21 @@
+package backjoon.step01;
+
+import java.util.Scanner;
+
+public class division {
+
+	public static void main(String[] args) {
+
+		int A;
+		int B;
+		
+		Scanner sc = new Scanner(System.in);
+		
+		A = sc.nextInt();
+		B = sc.nextInt();
+		
+		System.out.println((double)A/B);
+		
+	}
+
+}
