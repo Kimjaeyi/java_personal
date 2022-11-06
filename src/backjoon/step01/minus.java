7fcd529c761a2e54ffa193cfc4e40671e1519d3e
jjjy879 <jjjy879@naver.com>
2022-11-06 13:31:16 +0900

    숫자 입력 받아 뺄셈 연산

---
+++ src/backjoon/step01/minus.java
@@ -0,0 +1,20 @@
+package backjoon.step01;
+
+import java.util.Scanner;
+
+public class minus {
+	
+	public static void main(String[] args) {
+		
+		int A;
+		int B;
+		
+		Scanner sc = new Scanner(System.in);
+		
+		A = sc.nextInt();
+		B = sc.nextInt();
+		
+		System.out.println(A-B);
+	}
+	
+}
