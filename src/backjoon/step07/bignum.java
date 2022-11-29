226f383664276934a03e27a4a0314c36e0edd4cc
jjjy879 <jjjy879@naver.com>
2022-11-29 18:40:17 +0900

    미완

---
+++ src/backjoon/step07/bignum.java
@@ -0,0 +1,15 @@
+package backjoon.step07;
+
+import java.util.Scanner;
+
+public class bignum {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+	
+		
+	}
+
+}
