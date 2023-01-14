9999fad3d0ff05d2e1a35bc45ff1f8011c06452b
jjjy879 <jjjy879@naver.com>
2023-01-14 18:50:21 +0900

    배열 내 x보다 작은 수 출력

---
+++ src/backjoon/step04/xsmall.java
@@ -0,0 +1,27 @@
+package backjoon.step04;
+
+import java.util.Scanner;
+
+public class xsmall {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		int X = sc.nextInt();
+		
+		int array[] = new int[N];
+		
+		for (int i = 0; i < N; i++) {
+			array[i] = sc.nextInt();
+		}
+		
+		for (int i = 0; i < N; i++) {
+			if (array[i] < X) {
+				System.out.print(array[i] + " ");
+			}
+		}
+	}
+
+}
